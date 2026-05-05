f12fb85afaa20123f6f58f3269367fdbdf67acc8
Parthiban <rp.paarthi@gmail.com>
2026-05-05 12:49:13 +0530

    Day 5 - Database

--- django_project_1/blog/views.py
+++ django_project_1/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        "title": "Blog title 1",
-        "content": "Blog content 1",
-        "author": "Parthiban",
-        "date_posted": "01, May, 2026"
-    },
-    {
-        "title": "Blog title 2",
-        "content": "Blog content 2",
-        "author": "Parthi",
-        "date_posted": "02, May, 2026"   
-    }
-]
 
 def home(request):
     context = {
-        "posts": posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
